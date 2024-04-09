8dda48660d94724670addc85661a39d83fbd2cdc
javigh193 <115082160+javigh193@users.noreply.github.com>
2024-04-09 11:59:38 +0100

    Corregida validación de valores colores entre 0 y 255, no funciona bien el tema del array.forEach

--- 2.3.4/src/app/app.component.ts
+++ 2.3.4/src/app/app.component.ts
@@ -16,18 +16,28 @@ export class AppComponent {
 
   mixColor():void {
 
-    [this.red, this.green, this.blue].forEach(function(val, ind, arr) {
-        if(val > 255) {
-           arr[ind] = 255;
-        } else if (val < 0) {
-          arr[ind] = 0;
-        }
-    } );
-
-    var r = this.red.toString(16).padStart(2, '0');
-    var g = this.green.toString(16).padStart(2, '0');
-    var b = this.blue.toString(16).padStart(2, '0');
-    this.hexColor = '#' + r + g + b;
+  if(this.red > 255) {
+    this.red = 255;
+  } else if (this.red < 0) {
+    this.red = 0;
   }
 
+  if(this.green > 255) {
+    this.green = 255;
+  } else if (this.green < 0) {
+    this.green = 0;
+  }
+
+  if(this.blue > 255) {
+    this.blue = 255;
+  } else if (this.blue < 0) {
+    this.blue = 0;
+  }
+
+  var r = this.red.toString(16).padStart(2, '0');
+  var g = this.green.toString(16).padStart(2, '0');
+  var b = this.blue.toString(16).padStart(2, '0');
+  this.hexColor = '#' + r + g + b;
+  }
+  
 }
