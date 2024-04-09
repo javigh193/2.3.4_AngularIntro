003c362ee9b0d451d0053a7834bfec1235d0a4aa
javigh193 <115082160+javigh193@users.noreply.github.com>
2024-04-09 11:10:28 +0100

    Refactorización de la comprobación de los valores de cada color entre 0 y 255

--- 2.3.4/src/app/app.component.ts
+++ 2.3.4/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Color Mixer';
   version = 'v0';
@@ -14,22 +15,15 @@ export class AppComponent {
   hexColor:string="#000000";
 
   mixColor():void {
-    //Tarea pendiente, JS no admite pasar valor por referencia 
-    if (this.red > 255) {
-      this.red = 255;
-    } else if (this.red < 0) {
-      this.red = 0;
-    }
-    if (this.green > 255) {
-      this.green = 255;
-    } else if (this.green < 0) {
-      this.green = 0;
-    }
-    if (this.blue > 255) {
-      this.blue = 255;
-    } else if (this.blue < 0) {
-      this.blue = 0;
-    }
+
+    [this.red, this.green, this.blue].forEach(function(val, ind, arr) {
+        if(val > 255) {
+           arr[ind] = 255;
+        } else if (val < 0) {
+          arr[ind] = 0;
+        }
+    } );
+
     var r = this.red.toString(16).padStart(2, '0');
     var g = this.green.toString(16).padStart(2, '0');
     var b = this.blue.toString(16).padStart(2, '0');
